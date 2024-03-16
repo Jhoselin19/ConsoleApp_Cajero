35a3af2fcfbab695453e18ee4dccf8b6c4f7d209
Alumno <Alumno@LAB1504-18>
2024-03-16 12:30:32 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/CajeroAutomatico.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class CajeroAutomatico:CuentaBancaria
+    {
+
+        public void GenerarPIN()
+        {
+            Random rnd = new Random();
+            Pin = rnd.Next(1000, 10000);
+        }
+
+        public int ConsultarPIN()
+        {
+            return Pin;
+        }
+
+        public void CambiarPIN(int nuevoPIN)
+        {
+            Pin = nuevoPIN;
+            Console.WriteLine("PIN cambiado exitosamente.");
+        }
+
+        //CONSULTAR SALDO
+        public double ConsultarSaldo()
+        {
+            return Saldo;
+        }
+
+        //DEPOSITAR SALDO
+        public void Depositar(double deposito)
+        {
+            if (deposito > 0)
+            {
+                Saldo += deposito;
+                Console.WriteLine($"Se han depositado {deposito} en la cuenta.");
+            }
+            else
+            {
+                Console.WriteLine("La cantidad a depositar debe ser mayor que cero.");
+            }
+        }
+
+        //RETIRAR SALDO
+
+        public void Retirar(double retiro)
+        {
+            if(Saldo > retiro & retiro > 0)
+            {
+                Saldo -= retiro;
+                Console.WriteLine($"Se han retirado {retiro} de la cuenta.");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+// operacion = new Operacion();
+
+//persona.ValorA = 10;
+//persona.ValorB = 20;
+
+//int suma = persona.Sumar();
+//int resta = persona.Restar();
+
+//Console.WriteLine("El resultado de la suma es: ");
+//Console.WriteLine(suma);
+//Console.WriteLine("El resultado de la resta es: ");
+//Console.WriteLine(resta);
+
+//Console.WriteLine("Hello, World!");
+
+CajeroAutomatico cajero = new CajeroAutomatico();
+
+int PinAleatorio = cajero.ConsultarPIN();
+
+Console.WriteLine(cajero.ConsultarPIN());
+
+Console.WriteLine("Ingrese PIN: ");
+
+int pin = int.Parse(Console.ReadLine());
+
+
+if (pin == PinAleatorio)
+{
+    int opcion = 0;
+
+    while (opcion != 5)
+    {
+        Console.WriteLine("1) Depositar");
+        Console.WriteLine("2) Retirar");
+        Console.WriteLine("3) Cambiar PIN");
+        Console.WriteLine("4) Mostrar Saldo");
+        Console.WriteLine("5) Salir");
+
+        opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Ingrese el monto a depositar:");
+                double deposito = double.Parse(Console.ReadLine());
+                cajero.Depositar(deposito);
+                break;
+            case 2:
+                Console.WriteLine("Ingrese el monto a retirar:");
+                double retiro = double.Parse(Console.ReadLine());
+                cajero.Retirar(retiro);
+                break;
+            case 3:
+                Console.WriteLine("Ingrese el nuevo PIN:");
+                int nuevoPIN = int.Parse(Console.ReadLine());
+                cajero.CambiarPIN(nuevoPIN);
+                break;
+            case 4:
+                Console.WriteLine("Saldo actual: S/." + cajero.ConsultarSaldo());
+                break;
+            case 5:
+                Console.WriteLine("Saliendo del programa...");
+                break;
+            default:
+                Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
+                break;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("PIN incorrecto. Acceso denegado.");
+}
+
+//------------------------------------------------------
